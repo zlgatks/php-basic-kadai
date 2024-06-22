4133b8adb515c3bc7036a5c657cfb74581f38f65
Tsukasa <76234667+zlgatks@users.noreply.github.com>
2024-06-22 15:37:11 +0900

    課題005コミット
    ファイル登録

---
+++ kadai_005/score.php
@@ -0,0 +1,18 @@
+<?php
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+$sum = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+$ave = $sum / 10;
+echo "合計:{$sum}";
+echo '<br>';
+echo "平均:{$ave}";
+?>
